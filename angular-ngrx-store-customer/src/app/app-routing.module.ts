422b370f786a70e68a550bb3bd01c8716fb501d9
oopsmails <oopsmails@gmail.com>
2020-12-13 10:52:45 -0500

    20201213: merged final codes

--- angular-ngrx-store-customer/src/app/app-routing.module.ts
+++ angular-ngrx-store-customer/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "customers",
+    loadChildren: "../app/customers/customers.module#CustomersModule"
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+  declarations: []
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
