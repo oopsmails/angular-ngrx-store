1e0602be49f8a05f3925d891fb6c6cc7630d519a
oopsmails <oopsmails@gmail.com>
2021-01-10 15:46:22 -0500

    20210110: minor change for testing

--- angular-ngrx-store-customer/src/app/customers/customer.model.ts
+++ angular-ngrx-store-customer/src/app/customers/customer.model.ts
@@ -1,7 +1,36 @@
 export interface Customer {
   id?: number;
-  name: string;
-  phone: string;
-  address: string;
-  membership: string;
+  name?: string;
+  phone?: string;
+  address?: string;
+  membership?: string;
 }
+
+
+export enum PrintMedia {
+  Newspaper = 1,
+  Newsletter = 5,
+  Magazine = 6,
+  Book = 10
+}
+
+export interface User {
+  id?: number;
+  name?: string;
+  role?: string;
+  department?: string;
+}
+
+export enum Permission {
+  UserRead,
+  UserWrite,
+  UserExecute,
+  GroupRead,
+  GroupWrite,
+  GroupExecute,
+  AllRead,
+  AllWrite,
+  AllExecute,
+}
+
+
